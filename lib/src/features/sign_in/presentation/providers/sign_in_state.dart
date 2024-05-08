b68f05b70707e43a4d26a8104a8e7ec30543bd69
ProveY0urWorth <daniil091002@gmail.com>
2024-05-08 09:32:47 +0300

    Changed sign in state to equatable.

--- lib/src/features/sign_in/presentation/providers/sign_in_state.dart
+++ lib/src/features/sign_in/presentation/providers/sign_in_state.dart
@@ -1,18 +1,49 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:equatable/equatable.dart';
 import 'package:guider/src/features/sign_in/domain/validation/password_formz.dart';
 import 'package:guider/src/features/sign_in/domain/validation/username_formz.dart';
 
-part 'sign_in_state.freezed.dart';
-
-@freezed
-class SignInState with _$SignInState {
-  const factory SignInState({
-    @Default(UsernameFormz.pure()) UsernameFormz username,
-    @Default(PasswordFormz.pure()) PasswordFormz password,
-    @Default(false) bool formIsValid,
-    @Default(true) bool isPasswordObscured,
-    @Default(SignInStatus.initial) SignInStatus status,
-  }) = _SignInState;
-}
-
 enum SignInStatus { initial, sending, success, failure }
+
+class SignInState extends Equatable {
+  const SignInState({
+    this.username = const UsernameFormz.pure(),
+    this.password = const PasswordFormz.pure(),
+    this.formIsValid = false,
+    this.isPasswordObscured = true,
+    this.status = SignInStatus.initial,
+  });
+
+  final bool formIsValid;
+  final bool isPasswordObscured;
+  final PasswordFormz password;
+  final SignInStatus status;
+  final UsernameFormz username;
+
+  @override
+  List<Object?> get props => [
+        username,
+        password,
+        formIsValid,
+        isPasswordObscured,
+        status,
+      ];
+
+  @override
+  bool get stringify => true;
+
+  SignInState copyWith({
+    UsernameFormz? username,
+    PasswordFormz? password,
+    bool? formIsValid,
+    bool? isPasswordObscured,
+    SignInStatus? status,
+  }) {
+    return SignInState(
+      username: username ?? this.username,
+      password: password ?? this.password,
+      formIsValid: formIsValid ?? this.formIsValid,
+      isPasswordObscured: isPasswordObscured ?? this.isPasswordObscured,
+      status: status ?? this.status,
+    );
+  }
+}
